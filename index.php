b28c2f6c7ba61900348a8d4739d29a587d9dd6fb
hiroto.suka@rakuten-sec.co.jp <s-hiroto.suka@rakuten.com>
2021-08-20 10:22:57 +0900

    firstcommit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <title>tech boost</title>
+  <style>
+  h1 {
+    color: red;
+  }
+
+
+
+.description {
+  color: blue;
+}
+  </style>
+</head>
+  <body>
+
+<h1 id="title">Hello world!!!</h1>
+
+<p class="description">CSSの入門！</p>
+  </body>
+</html>
